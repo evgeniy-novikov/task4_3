43c2d8f89d7198d80609b6173b7358cd54c411e6
a10 <1_0 at usa.com>
2018-03-27 17:32:33 +0300

    Add set -e

--- task4_3.sh
+++ task4_3.sh
@@ -3,15 +3,21 @@
 E_NOARGS=75
 
 # Check options of runing command
-if test -z "$2"
+
+set -e
+if [ -z "$2" ] || ! [ -d "$1" ] || [ "$2" -le 0 ]
 then
 	echo "Usage: $0 path_to_dir backups_count"
-	# exit 1
 	exit $E_NOARGS
 fi
-
+set +e
 # Check and create backups folder
 
 BACKUPS_DIR=/tmp/backups/
+
+if  ! [ -d "$BACKUPS_DIR" ]
+then
+	mkdir "$BACKUPS_DIR"
+fi
 
 
