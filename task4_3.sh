269abd2f2c75444064abfd673dcaedf58abf42d3
a10 <1_0 at usa.com>
2018-03-27 15:02:06 +0300

    Fix in memory leak

--- task4_3.sh
+++ task4_3.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if [ -z "$2" ]
+E_NOARGS=75
+
+# Check options of runing command
+if test -z "$2"
 then
-	echo "Usage: `$0` path_to_dir backups_count"
+	echo "Usage: $0 path_to_dir backups_count"
+	# exit 1
 	exit $E_NOARGS
 fi
+
+# Check and create backups folder
 
 BACKUPS_DIR=/tmp/backups/
 
