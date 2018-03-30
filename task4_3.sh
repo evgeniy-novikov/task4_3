926398c39bc843837fc888331496ff6d1a843c81
a10 <1_0 at usa.com>
2018-03-30 12:44:52 +0300

    Add stderr messseges on error exit

--- task4_3.sh
+++ task4_3.sh
@@ -7,11 +7,13 @@ E_NOARGS=75
 if [ $# -ne 2 ] || ! [ -d "$1" ]
 then
 	echo "Usage: $0 path_to_dir backups_count"
+	>&2 echo "ERROR: wrong options"
 	exit $E_NOARGS
 elif ! [ 0 -lt "$2" ]
 then
 	echo "Usage: $0 path_to_dir backups_count"
 	echo "backups_count - must be a natural number"
+	>&2 echo "ERROR: wrong options"
 	exit $E_NOARGS
 fi
 START_PATH=$1
@@ -29,7 +31,7 @@ fi
 # Resolve PATH_TO_DIR
 
 CURRENT_PATH=$(pwd)
-PATH_TO_DIR=$(cd "$START_PATH" && pwd && cd "$CURRENT_PATH" || exit 1)
+PATH_TO_DIR=$(cd \""$START_PATH"\" && pwd && cd \""$CURRENT_PATH"\" || exit 1)
 echo "PATH_TO_DIR=$PATH_TO_DIR"
 
 # Get new backup base name
@@ -41,12 +43,12 @@ echo "TEMP_NAME=$TEMP_NAME"
 
 # Create tar gz
 
-tar -czf "$BACKUPS_DIR""$TEMP_NAME" "$PATH_TO_DIR"
+tar -czf \""$BACKUPS_DIR""$TEMP_NAME"\" \""$PATH_TO_DIR"\"
 
 # Check move exist backup
 
 ## EXIST_BACKUPS=($(echo /directory/*mystring*))
-EXIST_BACKUPS=($(ls -t "$BACKUPS_DIR"|grep  "$BASE_NAME[0-9].tar.gz"))
+EXIST_BACKUPS=($(ls -tr "$BACKUPS_DIR"|grep  "$BASE_NAME[0-9].tar.gz"))
 echo "len EXIST_BACKUPS=${#EXIST_BACKUPS[@]}"
 
 remove_old(){
@@ -65,6 +67,22 @@ fi
 
 ## remove_old $(( ${#EXIST_BACKUPS[@]} -  BACKUPS_COUNT ))
 
+switch_older(){
+	echo
+}
+
+
+switch_older 
+i=$(( "$BACKUPS_COUNT" - 1 ))
+
+
+
+
+
+for fff in $(ls -tr "$BACKUPS_DIR"|grep  "$BASE_NAME[0-9].tar.gz")
+do
+	echo "fff=$fff"
+done
 
 
 
